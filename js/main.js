a1f84328136a71774f1242adf341c098d9022b0f
Gabriel Chagas <gabrielserq@outlook.com>
2021-10-26 22:34:24 -0300

    chore: criando lógica para qunado o usuário descer a páginar, adicionar uma classe no header

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ const toggle = document.querySelectorAll('nav .toggle');
 toggle.forEach((item) =>{
   item.addEventListener('click', () =>{
     nav.classList.toggle('show')
-  })
-})
+  });
+});
 
 //* código para fechar o menu, quando um item é clicado
 
@@ -17,4 +17,18 @@ links.forEach(item => {
   item.addEventListener('click', () => {
     nav.classList.remove('show')
   });
-})
+});
+
+//* quando o usuário der o scroll, ele adiciona a classe scroll no header
+
+const header = document.querySelector('#header');
+
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY >= navHeight){
+    header.classList.add("scroll");
+  }else{
+    header.classList.remove("scroll");
+  }
+});
