ae4b1e9717f6d29b53403fe7f1530aa1a6b09fc4
Gabriel Chagas <gabrielserq@outlook.com>
2021-11-30 17:15:21 -0300

    chore: criando código para ativar o item do menu correspondente da sessão que o usuário está no momento

--- js/main.js
+++ js/main.js
@@ -69,10 +69,39 @@ function backToTop(){
   });
 }
 
+// menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('section[id]');
 
+function activateMenuAtCurrentSection(){
+
+
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+  
+  for(const section of sections){
+    
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+    console.log(sectionId);
+    
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+
+    console.log(checkpointStart, checkpointEnd, checkpoint)
+    
+    if(checkpointStart && checkpointEnd){
+      document.querySelector(`nav ul li a[href*=${sectionId}]`).classList.add('active');
+      console.log(sectionId)
+    }else{
+      document.querySelector(`nav ul li a[href*=${sectionId}]`).classList.remove('active');
+    }
+  }
+}
 
 // When Scroll
 window.addEventListener("scroll", () => {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
